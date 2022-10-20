857d9d7c586faebb23760c1a1109d0565ce23883
WavesFLL <58271000+WavesFLL@users.noreply.github.com>
2022-10-20 13:23:30 -0400

    Update nameMaker.py

--- nameMaker.py
+++ nameMaker.py
@@ -1,8 +1,8 @@
 import random
 
-adjectives=["dead","putrid","radioactive","Sam wins","Katie loses","glittery","smurfy","let it die","not-on-time","hot dog","fruit acid","didn't think","fabulous","edible","don't eat it","rocky"]
+adjectives=["dead","putrid","radioactive","Sam wins","Katie loses","glittery","smurfy","let it die","not-on-time","hot dog","fruit acid","didn't think","fabulous","edible","don't eat it","rocky","fantastic","spelled correctly","Sam loses","Katie wins","offended","in disguise"]
 
-nouns=["blue","yellow","magenta","green","brown","turquoise","white","red","orange","gray","black","scarlet","mustard","ketchup","pumpkin","who-knows","dirt","purple","violet","indigo","maroon","skunk","giraffe","baby","insect","cell tissue","garbage","lorax"]
+nouns=["blue","yellow","magenta","green","brown","turquoise","white","red","orange","gray","black","scarlet","mustard","ketchup","pumpkin","who-knows","dirt","purple","violet","indigo","maroon","skunk","giraffe","baby","insect","cell tissue","garbage","lorax","sparkly pink"]
 
 def getName():
     value1 = random.randint(0, len(adjectives)-1)
